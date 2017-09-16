7e18b41a78085a0dca67c9c3e75f279e3b12edff
kangle0224 <kangle0224@gmail.com>
2017-09-16 22:59:48 +0800

    test

---
+++ s14day19/kl/gt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import chardet
+import codecs
+
+def readfile(filename):
+    with open(filename) as f:
+        data = f.readlines()
+    return data
+f1 = r'e:\a.txt'
+f2 = r'e:\b.txt'
+
+a1 = readfile(f1)
+a2 = readfile(f2)
+#
+# for i in a1:
+#     print i,
+#
+# print '\n-----------'
+# for i in a2:
+#     print i.decode('gbk').encode('utf-8'),
+
+# for i in a1:
+#     print chardet.detect(i)
+
+# f3 = codecs.open(f2,mode='rb',encoding='gbk').readlines()
+# for i in f3:
+#     print i
+
+
